a0952cf0ca413cddd2bb8cbdb717e6b9cc9ad575
tanishqtiwari25 <tanishqtiwari265@gmail.com>
2026-09-05 17:44:31 +0530

    update vite,app

--- montra-frontend/vite.config.js
+++ montra-frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ command }) => ({
+  // Production me GitHub repo name aur local me root path
   base: command === 'build' ? '/Montra-Money-Manager/' : '/',
 
   plugins: [react()],
